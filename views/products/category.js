66a7dabf034f6e18cd201bce14070faa17f5a5aa
alsgur39 <alsgur39@gmail.com>
2024-02-20 22:14:51 +0900

    Fix: 상품 리스트가 안불러와지는 오류

--- views/products/category.js
+++ views/products/category.js
@@ -1,6 +1,6 @@
 function getCategory(id) {
   // TODO: API를 호출해서 카테고리 정보를 가져와서 반환합니다.
-
+  
   return {
     name: 'American',
   }
@@ -27,14 +27,14 @@ function updateCategoryTitle() {
     alert('카테고리 이름을 표시할 요소를 찾을 수 없습니다.')
     return
   }
-
+  
   // h1태그 안의 내용을 카테고리 이름으로 바꾼다
   categoryTitleH1.innerText = category.name;
 }
 
 function getProductList() {
   // TODO: API를 호출해서 상품 목록을 가져와서 반환합니다.
-
+  
   // 임시 Product 데이터
   return [
     { id: 1, name: 'Burger', price: 10000 },
@@ -52,33 +52,23 @@ function updateProductList() {
     alert('상품 목록을 표시할 요소를 찾을 수 없습니다.')
     return
   }
-
   const productList = getProductList(); // API 호출해서 상품 목록 가져오기 => 서버를 호출하니까 느림
-
-  function updateProductList() {
-    const productListElement = document.querySelector('ul.product-list');
-    if (!productListElement) {
-      alert('상품 목록을 표시할 요소를 찾을 수 없습니다.')
-      return
-    }
-
-    const productList = getProductList(); // API 호출해서 상품 목록 가져오기 => 서버를 호출하니까 느림
-
-    productListElement.innerHTML = '';
-    for (let i = 0; i <= 16 && i < productList.length; i++) {
-      const product = productList[i];
-      productListElement.innerHTML += `
-        <li class="product-item">
-            <a href="#">
-                <img src="https://picsum.photos/300/300" alt="제품 이미지">
-                <h2>${product.name}</h2>
-                <span class="price">${product.price}</span>
-            </a>
-        </li>
-      `;
-    }
+  
+  productListElement.innerHTML = '';
+  for (let i = 0; i <= 16 && i < productList.length; i++) {
+    const product = productList[i];
+    productListElement.innerHTML += `
+    <li class="product-item">
+    <a href="#">
+    <img src="https://picsum.photos/300/300" alt="제품 이미지">
+    <h2>${product.name}</h2>
+    <span class="price">${product.price}</span>
+    </a>
+    </li>
+    `;
   }
 }
+
 window.addEventListener('load', () => {
   // 페이지가 로드되면 카테고리 이름을 업데이트한다
   updateCategoryTitle();
