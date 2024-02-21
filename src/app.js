fc6111d7680e9c8846c609ed900a47b4c4985a9c
Saeyoung Choi <effysogood@gmail.com>
2024-02-21 16:00:46 +0900

    Feat: books API

--- src/app.js
+++ src/app.js
@@ -6,10 +6,16 @@ require('dotenv').config();
 const bookRouter = require('./routers/bookRouter');
 
 //connect to mongodb
-mongoose.connect(process.env.MONGO_URI);
+// TODO : env 연결 확인
+// mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(
+  'mongodb+srv://butterandbetter:Uqt93SbIF7GNPOXM@main.xqqect0.mongodb.net/BAB'
+);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => { console.log('Connected to MongoDB') });
+db.once('open', () => {
+  console.log('📍 Connected to MongoDB');
+});
 
 const app = express();
 
@@ -19,30 +25,25 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    res.send('Butter and Better')
+  res.send('Butter and Better');
 });
 
 app.use('/api/books', bookRouter);
 
-
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    next(createError(404));
+app.use(function (req, res, next) {
+  next(createError(404));
 });
-  
+
 // error handler
-app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-  
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
-app.listen(3000, () => {
-    console.log('port 3000')
-});
-
-
+module.exports = app;
