881e8d8facf385ba2e4d411045dd82f5e1c13b91
alsgur39 <alsgur39@gmail.com>
2024-02-21 14:34:40 +0900

    Refactor: loadHTML리팩토링

--- views/public/loadHTML.js
+++ views/public/loadHTML.js
@@ -1,13 +1,23 @@
-function loadHTML(file) {
-    var rawFile = new XMLHttpRequest();
-    rawFile.open("GET", file, false);
-    rawFile.onreadystatechange = function () {
+function loadHTML(file, targetElement) {
+  var rawFile = new XMLHttpRequest();
+  rawFile.open("GET", file, true);
+  rawFile.onreadystatechange = function () {
       if (rawFile.readyState === 4) {
-        if (rawFile.status === 200 || rawFile.status == 0) {
-          var allText = rawFile.responseText;
-          document.write(allText);
-        }
+          if (rawFile.status === 200 || rawFile.status == 0) {
+              var allText = rawFile.responseText;
+            //   var targetElement = document.getElementById(targetElementId);
+
+              // 생성된 div를 목표 요소에 추가
+              targetElement.innerHTML= allText;
+          }
       }
-    };
-    rawFile.send(null);
+  };
+  rawFile.send(null);
 }
+
+window.addEventListener('load', () => {
+    document.querySelectorAll('[data-load-html]').forEach((element) => {
+        const filePath = element.dataset.loadHtml
+        loadHTML(filePath, element)
+    })
+})
