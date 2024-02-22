a50d7e53ec0fac6b7e4103e3a828e11fceadbbbc
KiTaek-Lim <lkt95s@naver.com>
2024-02-22 17:09:28 +0900

    Feat: admin-category-추가버튼 구현

--- views/admin/category/category.js
+++ views/admin/category/category.js
@@ -2,17 +2,22 @@ document.addEventListener('DOMContentLoaded', function () {
   // 모달 열기 버튼과 모달 가져오기
   var openModalBtn1 = document.getElementById("openModalbtn1");
   var openModalBtn2 = document.getElementById("openModalbtn2");
+  var openModalBtn3 = document.getElementById("openModalbtn3");
   var modal1 = document.getElementById("myModal1");
   var modal2 = document.getElementById("myModal2");
+  var modal3 = document.getElementById("myModal3");
   
-  // 모달 열기 버튼에 이벤트 리스너 추가
+  // 모달 열기 버튼에 이벤트 리스너 추가  
   openModalBtn1.onclick = function () {
     modal1.style.display = "flex";
   };
   openModalBtn2.onclick = function () {
     modal2.style.display = "flex";
+  }
+  openModalBtn3.onclick = function () {
+    modal3.style.display = "flex";
   };
-
+    
 
   // 모달 닫기 함수
   function closeModal1() {
@@ -25,6 +30,17 @@ document.addEventListener('DOMContentLoaded', function () {
       closeModal1();
     }
   };
+  // 모달 닫기 함수(modal3)
+  function closeModal3() {
+    modal3.style.display = "none";
+  }
+  
+  // 외부 클릭 시 모달 닫기(modal3)
+  window.onclick = function (event) {
+    if (event.target == modal3) {
+      closeModal3();
+    }
+  };
   
   // 저장 버튼에 이벤트 리스너 추가
   var saveBtn1 = document.getElementById("save-Btn1");
@@ -33,6 +49,13 @@ document.addEventListener('DOMContentLoaded', function () {
     alert("저장되었습니다.");
     modal1.style.display = "none";
   };
+
+  var saveBtn3 = document.getElementById("save-Btn3");
+  saveBtn3.onclick = function () {
+    // 저장 버튼을 눌렀을 때 할 작업을 추가
+    alert("추가되었습니다.");
+    modal3.style.display = "none";
+  }
   
   // 삭제 버튼과 취소 버튼에 이벤트 리스너 추가
   var deleteBtn = document.getElementById("delete-Btn");
