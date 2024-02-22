d8953c9bb9503c43af821d095e48afdb009b9d20
zizonz <wlswn2228@naver.com>
2024-02-22 17:51:50 +0900

    Feat:books api 수정

--- src/routers/bookRouter.js
+++ src/routers/bookRouter.js
@@ -1,112 +1,145 @@
 const express = require('express');
 const router = express.Router();
-const { Book } = require('../db/models/book-model');
+const { Book } = require('../db/models/bookModel');
 const asyncHandler = require('../utils/async-handler');
 
 // 상품 목록 조회
-router.get('/books', asyncHandler(async (req, res, next) => {
-  try {
+router.get(
+  '/',
+  asyncHandler(async (req, res, next) => {
     const page = Number(req.query.page || 1);
     const perPage = Number(req.query.perPage || 24);
 
     const [total, books] = await Promise.all([
       Book.countDocuments({}),
       Book.find({})
-        .sort({ createdAt: -1 })
+        //.sort({ createdAt: -1 })
         .skip(perPage * (page - 1))
-        .limit(perPage)
+        .limit(perPage),
     ]);
     const totalPage = Math.ceil(total / perPage);
 
     res.status(200).json({
-        "status": 200,
-        "msg": "상품 목록 조회 완료",
-        data: {
-          totalPage: totalPage,
-          productCount: total,
-          books,
-        },
-      });
-      }
-catch (error) {
-    next(error);
-  }
-}));
+      status: 200,
+      msg: '상품 목록 조회 완료',
+      data: {
+        totalPage: totalPage,
+        productCount: total,
+        books,
+      },
+    });
+  })
+);
 
 // 상품 상세 조회
-router.get('/books/:id', asyncHandler(async (req, res, next) => {
-  const _id = req.params.id;
-
-  try {
+router.get(
+  '/:id',
+  asyncHandler(async (req, res, next) => {
+    const _id = req.params.id;
     const book = await Book.findById(_id);
 
     res.status(200).json({
-      "status": 200,
-       msg: "상품 상세 조회 성공",
-       data: book,
+      status: 200,
+      msg: '상품 상세 조회 성공',
+      data: book,
     });
-  } catch (error) {
-    next(error);
-  }
-}));
+  })
+);
 
 // 상품 추가 *admin
-router.post('/books', asyncHandler(async (req, res, next) => {
-  try {
-    const newbook = new Book(req.body);
-    await newbook.save();
+// router.post(
+//   '/admin',
+//   asyncHandler(async (req, res, next) => {
+//     try {
+//       const newbook = new Book(req.body);
+//       await newbook.save();
 
-    res.status(201).json({
-      status: 201,
-      message: '상품 추가 완료',
-      data: newbook,
-    });
-  } catch (error) {
-    next(error);
-  }
+//       res.status(201).json({
+//         status: 201,
+//         message: '상품 추가 완료',
+//         data: newbook,
+//       });
+//     } catch (error) {
+//       next(error);
+//     }
+//   })
+// );
+
+router.post('/admin', asyncHandler(async(req, res, next) => {
+  const { title, category_id, price, content, img_url, is_sale, quantity, rating } = req.body;
+  await Book.create({
+    title,
+    category_id,
+    price,
+    content,
+    img_url,
+    is_sale,
+    quantity,
+    rating
+  });
+  res.status(201).json({
+    status: 201,
+    msg: '상품 추가 완료'
+  });
 }));
 
 // 상품 삭제 *admin
-router.delete('/books/:id', asyncHandler(async (req, res, next) => {
-  try {
-    const deletedBook = await Book.findByIdAndDelete(req.params.id);
+// router.delete(
+//   '/admin/:id',
+//   asyncHandler(async (req, res, next) => {
+//     try {
+//       const deletedBook = await Book.findByIdAndDelete(req.params.id);
 
+//       res.status(200).json({
+//         status: 200,
+//         message: '상품 삭제 완료',
+//         data: {
+//           deletedBook,
+//         },
+//       });
+//     } catch (error) {
+//       next(error);
+//     }
+//   })
+// );
+
+router.delete('/admin/:id', asyncHandler (async(req, res, next) => {
+  const id = req.params.id;
+  await Book.deleteOne({ _id: id });
     res.status(200).json({
       status: 200,
-      message: '상품 삭제 완료',
-      data: {
-        deletedBook,
-      },
+      msg: '상품 삭제 완료'
     });
-  } catch (error) {
-    next(error);
-  }
 }));
 
 // 상품 수정 *admin
-router.patch('/books/:id', asyncHandler(async (req, res, next) => {
-  const updates = Object.keys(req.body);
-  const allowedUpdates = ['name', 'category_name', 'price', 'content', 'img_url', 'is_sale', 'quantity', 'rate'];
-  const isValidUpdate = updates.every((update) => allowedUpdates.includes(update));
+router.patch(
+  '/admin/:id',
+  asyncHandler(async (req, res, next) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = [
+      'name',
+      'category_name',
+      'price',
+      'content',
+      'img_url',
+      'is_sale',
+      'quantity',
+      'rate',
+    ];
 
-  if (!isValidUpdate) {
-    return res.status(400).send({ error: 'Invalid updates!' });
-  }
+    const updatedBookInfo = await Book.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true, runValidators: true }
+    );
 
-  try {
-    const updatedBookInfo = await Book.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
-
-    
     res.status(200).json({
       status: 200,
       message: '상품 수정 완료',
       data: updatedBookInfo,
     });
-  } catch (error) {
-    next(error);
-  }
-}));
-
-
+  })
+);
 
 module.exports = router;
