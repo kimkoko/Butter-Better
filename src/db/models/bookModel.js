f7a0bfa301424fbe7004818e86a4e658dcef16cf
zizonz <wlswn2228@naver.com>
2024-02-22 18:47:03 +0900

    Feat:books api 추가

--- src/db/models/bookModel.js
+++ src/db/models/bookModel.js
@@ -2,8 +2,3 @@ const mongoose = require('mongoose');
 const BookSchema = require('../schemas/bookSchema');
 
 exports.Book = mongoose.model('Book', BookSchema);
-
-const mongoose = require('mongoose');
-const BookSchema = require('../schemas/bookSchema');
-
-exports.Book = mongoose.model('Book', BookSchema);
