2a0f7446a8d66e41f4e1cb0efa8b825244a95799
alsgur39 <alsgur39@gmail.com>
2024-02-23 09:32:21 +0900

    Feat: api 연결을 위한 코드 수정

--- views/detail/detail.js
+++ views/detail/detail.js
@@ -1,58 +1,82 @@
-function getProductInfo() {
-  // TODO: API를 호출해서 상품 목록을 가져와서 반환합니다.
-  
+import { API_HOST } from '../common/api.js';
+
+async function getProductInfo(id) {
+  // TODO: API를 호출해서 상품 상세 정보를 가져와서 반환합니다.
+  const apiUrl = `${API_HOST}/api/books/${id}`;
+
+  // fetch를 사용하여 GET 요청 보내기
+  const productDetail = fetch(apiUrl)
+    .then(response => {
+      // 응답을 JSON 형식으로 파싱
+      return response.json();
+    })
+    .then(res => {
+      // 성공적으로 데이터를 받았을 때 처리
+      console.log(res);
+      
+    })
+    .catch(error => {
+      // 오류 처리
+      console.error('API 호출 중 오류 발생:', error);
+    });
+
+
+  return productDetail
+
   // 임시 Product 데이터
-  return [
-    { 
-      id: 1,
-      name: 'Burger',
-      price: 20000,
-      content: "상품 설명",
-      category: "East",
-      starts: 3,
-    }
-  ]
+  // return { 
+  //   _id: "1",
+  //   title: 'Burger',
+  //   category_id: {
+  //     _id:"10",
+  //     name:"test",
+  //     sort: 10,
+  //   },
+  //   price: "10,000",
+  //   content: "상품 설명",
+  //   img_url: "https://picsum.photos/300/300",
+  //   quantity: 3,
+  //   rating: 4,
+  //   __v: 0
+  // }
 }
 
-
 /* 책 정보 */
-function updateProductInfo() {
+async function updateProductInfo() {
   const productInfoElement = document.querySelector('.mainBox .inner .topBox > div');
   if (!productInfoElement) {
     alert('상품을 표시할 수 없습니다.')
     return
   }
-  const productInfo = getProductInfo(); // API 호출해서 상품 목록 가져오기 => 서버를 호출하니까 느림
+
+  const product = await getProductInfo(); // API 호출해서 상품 상세 정보 가져오기 => 서버를 호출하니까 느림
   
-  productInfo.forEach(product => {
-      
-      productInfoElement.innerHTML = `
-        <div class="bookImg">
-          <p class="img"><img src="https://picsum.photos/300/300" alt="책 표지"></p>
-        </div>
-
-        <div class="bookInfo">
-          <h2 class="bookName">${product.name}</h2>
-            
-          <div class="option">
-            <p class="category">${product.category}</p>
-            <p class="stars">
-              <ion-icon name="star"></ion-icon>
-              <span class="num">${product.starts}</span>
-            </p>
-          </div>
-
-          <div class="bookText">
-            ${product.content}
-          </div>
-
-          <div class="priceButton">
-            <p class="price">￦ ${product.price.toLocaleString()}</p>
-            <button type="button">ADD TO CART</button>
-          </div>
-        </div>
-      `;
-  })
+  productInfoElement.innerHTML = `
+  <div class="bookImg">
+  <p class="img"><img src="${product.img_url}" alt="책 표지"></p>
+  </div>
+  
+  <div class="bookInfo">
+  <h2 class="bookName">${product.title}</h2>
+  
+  <div class="option">
+  <p class="category">${product.category_id.name}</p>
+  <p class="stars">
+  <ion-icon name="star"></ion-icon>
+  <span class="num">${product.rating}</span>
+  </p>
+  </div>
+  
+  <div class="bookText">
+  ${product.content}
+  </div>
+  
+  <div class="priceButton">
+  <p class="price">￦ ${product.price}</p>
+  <button type="button">ADD TO CART</button>
+  </div>
+  </div>
+  `;
 }
 
 
@@ -63,36 +87,36 @@ window.addEventListener('load', () => {
 
 function getRecommendsList() {
   // TODO: API를 호출해서 상품 목록을 가져와서 반환합니다.
-
+  
   // 임시 Recommends 데이터
   return [
-      { id: 1, name: 'Recommends1', price: '10,000' },
-      { id: 2, name: 'Team4 fighting', price: '50,000' },
-      { id: 3, name: '정신나가정신나가', price: '30,000' },
-      { id: 4, name: '정신들어와정신들어와', price: '70,000' },
+    { id: 1, name: 'Recommends1', price: '10,000' },
+    { id: 2, name: 'Team4 fighting', price: '50,000' },
+    { id: 3, name: '정신나가정신나가', price: '30,000' },
+    { id: 4, name: '정신들어와정신들어와', price: '70,000' },
   ];
 }
 
 function updateRecommendsList() {
   const RecommendsListElement = document.querySelector('ul.Recommends-list');
   if (!RecommendsListElement) {
-      alert('상품 목록을 표시할 요소를 찾을 수 없습니다.');
-      return;
+    alert('상품 목록을 표시할 요소를 찾을 수 없습니다.');
+    return;
   }
-
+  
   const RecommendsList = getRecommendsList(); // API 호출해서 상품 목록 가져오기 => 서버를 호출하니까 느림
-
+  
   RecommendsListElement.innerHTML = '';
   for (const Recommends of RecommendsList) {
     RecommendsListElement.innerHTML += `
-          <li class="Recommends-item">
-              <a href="#">
-                  <img src="https://picsum.photos/300/300" alt="제품 이미지">
-                  <h2>${Recommends.name}</h2>
-                  <span class="Recommends-price">${Recommends.price}</span>
-              </a>
-          </li>
-      `;
+    <li class="Recommends-item">
+    <a href="#">
+    <img src="https://picsum.photos/300/300" alt="제품 이미지">
+    <h2>${Recommends.name}</h2>
+    <span class="Recommends-price">${Recommends.price}</span>
+    </a>
+    </li>
+    `;
   }
 }
 
