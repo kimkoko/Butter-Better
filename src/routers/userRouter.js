bf4e394a4e44dfb39e389f0e6ba96a347ba1c4b9
[SW8] 김송이 <songikim4464@gmail.com>
2024-02-23 14:15:00 +0000

    Feat: user API - 로그인 시 토큰 생성, loginRequired 미들웨어 적용 시도

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const { User } = require('../db/models/userModel');
 const hashPassword = require('../utils/hash-password');
 const asyncHandler = require('../utils/async-handler');
+const jwt = require('jsonwebtoken');
+const loginRequired = require('../middlewares/loginRequired');
 
 router.get('/', async(req, res, next) => {
     res.send('users page.')
@@ -10,7 +12,8 @@ router.get('/', async(req, res, next) => {
 
 // 회원가입 
 router.post('/register', asyncHandler(async(req, res, next) => {
-    const { name, password, email, phone } = req.body;
+    const { name, password, email, phone  } = req.body;
+    const { postcode, main, detail } = req.body.address;
     const user = await User.findOne({ email });
     if(user) {
         throw new Error('이미 가입된 회원입니다.');
@@ -21,6 +24,8 @@ router.post('/register', asyncHandler(async(req, res, next) => {
         password: hashedPassword,
         email,
         phone,
+        address: {postcode, main, detail},
+        deleted_at: null,
         is_admin: false
     });
     res.status(201).json({
@@ -37,12 +42,29 @@ router.post('/login', asyncHandler (async (req, res, next) => {
     if(correctPassword !== hashPassword(password)) {
         throw new Error('비밀번호가 일치하지 않습니다.');
     }
+    let token = jwt.sign({
+        type: 'JWT',
+        email: email,
+    }, process.env.SECRET_KEY);
+    res.cookie("token", token);
+    res.status(201).json({
+        status:201,
+        msg: "로그인 완료"
+    })
 
+}))
+
+router.get('/mypage', loginRequired, asyncHandler(async (req, res, next) => {
+    res.send('users mypage');
 }))
 
 router.delete('/:email', asyncHandler(async (req, res) => {
     const _email = req.params.email;
     await User.deleteOne({ email: _email });
+    res.status(200).json({
+        status: 200,
+        msg: '회원 삭제 완료'
+    });
 }));
 
 module.exports= router;
