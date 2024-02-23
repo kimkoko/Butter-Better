8a760cadca9f42ef2f9071210f7e7c4ea80df42d
zizonz <wlswn2228@naver.com>
2024-02-23 14:25:42 +0900

    Feat:카테고리 api 추가

--- src/services/categoryService.js
+++ src/services/categoryService.js
@@ -1,14 +1,15 @@
-const categoryModel = require('../db/models/categoryModel')
+const { Category } = require('../db/models/categoryModel')
 // const 
 
 class CategoryService {
 
-    constructor(categoryModel) {
-        this.categoryModel = categoryModel
+    constructor(Category) {
+        this.Category = Category
     }
+
  // 카테고리 조회
     async getCategories () {
-        return await this.categoryModel.findAll()
+        return await this.Category.find({})
     }
 
  // 카테고리 추가
@@ -16,35 +17,33 @@ class CategoryService {
         const { category } = categoryInfo;
 
         // 중복 확인
-        const categoryName = await this.categoryModel.findByCategory(category)
+        const categoryName = await this.Category.findOne({ name : category })
         if (categoryName) {
             throw new Error('이미 있는 카테고리입니다.')
         }
         // db 저장
-        return await this.categoryModel.create(categoryInfo)
+        return await this.Category.create(categoryInfo)
     }
 
- // 카테고리 수정
+ // 카테고리 수정 shortId 불가능하면 수정해야 될듯 합니다.
     async setCategory(categoryId, toUpdate) {
-        let category = await this.categoryModel.findById(categoryId)
+        const category = await this.Category.findById({ id : categoryId })
         if (!category) {
             throw new Error('해당 카테고리의 id가 없습니다.')
         }
         // 중복 확인
-        const categoryName = await this.categoryModel.findByCategory(toUpdate.category)
+        const categoryName = await this.Category.findOne({ name : toUpdate.category })
         if (categoryName) {
             throw new Error ('이미 있는 카테고리입니다.')
         }
 
-        return await this.categoryModel.update({
-            categoryId,
-            update: toUpdate,
-        })
+        return await this.Category
+        .findOneAndUpdate({ id : categoryId }, { toUpdate })
     }
 
- // 카테고리 조회
+ // 카테고리 삭제
     async deleteCategory(categoryId) {
-        let category = await categoryModel.delete(categoryId);
+        let category = await Category.findOneAndDelete({ id : categoryId });
         if (!category) {
             throw new Error('해당 카테고리의 id가 없습니다.')
         }
@@ -52,5 +51,7 @@ class CategoryService {
         return category
     }
 }
+
+const categoryService = new CategoryService(Category);
 
 module.exports = CategoryService
