49f946e5ade12e26146f34141c4c5f01662a85aa
zizonz <wlswn2228@naver.com>
2024-02-24 19:09:03 +0900

    Feat:bookService 추가

--- src/routers/viewRouter.js
+++ src/routers/viewRouter.js
@@ -3,6 +3,7 @@ const router =  express.Router();
 const path = require('path');
 
 router.use('/', servestatic('home'));
+router.use('/:id', servestatic('detail'));
 
 function servestatic (resource) {
     const resourcePath = path.join(__dirname, `../views/${resource}`);
