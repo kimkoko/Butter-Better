3e72f77de36351d37a7b64617c5b7d9230876f94
alsgur39 <alsgur39@gmail.com>
2024-02-26 23:03:37 +0900

    Feat: 관리자 상품정보 수정하기

--- src/views/admin/products/products.js
+++ src/views/admin/products/products.js
@@ -1,3 +1,22 @@
+// {
+//   '65dcasdlkfjwaioef': {
+//     title: '책 제목',
+//     content: '책 내용',
+//     ...
+//   },
+//   '12312312123asfwef': {
+//     title: '책 제목',
+//     content: '책 내용',
+//     ...
+//   }
+// }
+let products = {}
+
+// ID로 현재 가지고 있는 제품들 중에서 찾기
+function findProductById(id) {
+  return products[id];
+}
+
 document.addEventListener('DOMContentLoaded', function () {
   // 모달 열기 버튼과 모달 가져오기
   const addBtn = document.getElementById("add-btn");
@@ -62,14 +81,59 @@ function connectModalEvent() {
               pargraph.innerText = "상품 수정";
           }
           modal1.style.display = "flex";
+
+          // TODO: 모달의 내용을 해당하는 제품의 내용으로 수정
+          updateModalContent(modalEditBtn);
       });
   });
 
   modalDeleteBtns.forEach(function (modalDeleteBtn) {
-      modalDeleteBtn.addEventListener("click", function () {
-          modal2.style.display = "flex";
+    // data-image-url="ImageURL" data-product-id="asdf"
+    // console.log(modalDeleteBtn.dataset)
+    console.log(modalDeleteBtn.dataset.productId)
+
+    modalDeleteBtn.addEventListener("click", function () {
+        modal2.style.display = "flex";
+
+        // TODO: 현재 제품을 삭제하는 작업 추가
+        // 그럴려면 현재 제품이 뭔지 알아야됨
+        // => 현재 제품을 ID로 알아내기
+        const productId = modalDeleteBtn.dataset.productId;
+        if (!productId) return;
+        const product = findProductById(productId);
+        console.log(product);
+        console.log(product.title);
       });
   });
+}
+
+// saveBtn1.addEventListener("click", editProduct);
+
+// 수정 모달의 저장 버튼을 눌렀을 때 실행되는 함수
+function editProduct() {
+  // 수정 버튼을 눌렀을 때 할 작업을 추가
+  // TODO: API로 수정 요청을 보내는 작업 추가
+  // 이 API에서 수정된 정보를 줌 (하나의 Product에 대해서)
+  // => 그 정보를 가지고 화면에 있는 정보를 수정 (tr 한개의 내용만 수정)
+  alert("수정되었습니다.");
+  modal1.style.display = "none";
+
+  renderProductList();
+}
+
+function deleteProduct() {
+  // 삭제 버튼을 눌렀을 때 할 작업을 추가
+
+  // 삭제요청 API
+  fetch(`${API_HOST}/api/books/${productId}`, {
+    method: 'DELETE',
+  }).then(() => {
+    // OK
+    renderProductList();
+  }).catch(() => {
+    // Error
+    alert('삭제 실패')
+  })
 }
 
 // 제품 리스트 가져오기 //
@@ -87,21 +151,31 @@ async function renderProductList() {
     }
     
     const data = await response.json();
-    console.log(data)
-    const products = data.data.books
+    console.log('response: ', data)
+    // products = data.data.books // 이건 배열
     
+    // => 오브젝트로 변경
+    products = {};
+    data.data.books.forEach((book) => {
+      products[book._id] = book;
+    });
+    
+    console.log('원래 데이터: ', data.data.books)
+    console.log('오브젝트: ', products)
+    // 하나의 예시
+    // console.log('임의의 오브젝트 하나 찾아봄 ("65dc25d85d51b0a7ba1cc1a2"): ', products["65dc25d85d51b0a7ba1cc1a2"])
     const productListElement = document.getElementById('table-List');
     // const categoryName = document.getElementById("categoryName")
-    
+    // console.log(Object.entries(products))
     
     // 상품 데이터를 기반으로 상품 리스트 생성
-    products.forEach(product => {
+    for (const [id, product] of Object.entries(products)) {
       const row = document.createElement('tr');
       row.classList.add('products-list');
       row.innerHTML = `
       <td>${product.isBestSeller}</td>
       <td>${product.title}</td>
-      <td>${product.category_id.name}</td>
+      <td>${product.category}</td>
       <td class="content">${product.content}</td>
       <td>${product.price.toLocaleString()}</td>
       <td class="img">${product.img_url}</td>
@@ -109,11 +183,17 @@ async function renderProductList() {
       <td>${product.rate}</td>
       <td>
       <button id="openModalbtn1" class="modal-edit-btn">수정</button>
-      <button id="openModalbtn2" class="modal-delete-btn">삭제</button>
+      <button 
+        id="openModalbtn2"
+        class="modal-delete-btn"
+        data-product-id="${product._id}"
+      >
+        삭제
+      </button>
       </td>`;
       
       productListElement.appendChild(row);
-    });
+    };
     //모달 불러오기
     connectModalEvent();
 
