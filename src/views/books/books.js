58977dc49c09a66200cb00d88ee4c0c864af83bf
Saeyoung Choi <effysogood@gmail.com>
2024-02-28 14:22:21 +0900

    Feat: 전체 상품 페이지 분리

---
+++ src/views/books/books.js
@@ -0,0 +1,39 @@
+import { API_HOST } from '../common/api.js';
+
+/* All Books */
+async function getAllBooks() {
+  try {
+    // 서버에서 모든 책 목록을 가져오는 API 호출
+    const response = await fetch(`${API_HOST}/api/books`);
+
+    // API 응답 데이터를 JSON 형태로 변환
+    const allBooks = await response.json();
+    console.log('모든 책 목록:', allBooks);
+
+    // 여기서 책 목록을 활용하여 원하는 작업을 수행할 수 있습니다.
+    const productListElement = document.getElementById('productList');
+
+    allBooks.slice(0, 24).forEach((book) => {
+      // 상품 요소 생성
+      const productItem = document.createElement('li');
+      productItem.classList.add('product-item');
+      productItem.innerHTML = `
+            <a href="/src/views/detail/detail.html?id=${book._id}">
+                <p class="img"><img src="${book.img_url}" alt="제품 이미지"></p>
+                <h2>${book.title}</h2>
+                <span class="price">${book.price.toLocaleString()} 원</span>
+            </a>
+        `;
+
+      // 상품 리스트에 상품 추가
+      productListElement.appendChild(productItem);
+    });
+  } catch (error) {
+    console.error('모든 책 목록을 조회하는 중 오류가 발생했습니다:', error);
+  }
+}
+
+// 페이지가 로드되면 상품 리스트 렌더링
+window.addEventListener('load', () => {
+  getAllBooks();
+});
--- src/views/category/category.js
+++ src/views/category/category.js
@@ -4,42 +4,42 @@ import { API_HOST } from '../common/api.js';
 async function getCategoryBooks() {
   try {
     // categoryId 가져오는 함수
-    function getCategoryIdFromUrl(){
+    function getCategoryIdFromUrl() {
       const url = new URL(window.location.href);
-      return url.searchParams.get("category");
+      return url.searchParams.get('category');
     }
-  
+
     // categoryId 가져오기
     const categoryId = getCategoryIdFromUrl();
     console.log('categoryId: ', categoryId);
 
     // categoryId를 사용하여 카테고리 정보를 가져오는 API 호출
-    const response = await fetch(`${API_HOST}/api/categories/books/${categoryId}`);
-    
+    const response = await fetch(
+      `${API_HOST}/api/categories/books/${categoryId}`
+    );
+
     // API 응답 데이터를 JSON 형태로 변환
     const categoryBooks = await response.json();
     console.log('categoryBooks: ', categoryBooks);
-    const categoryBook = categoryBooks.data.books
-    
+    const categoryBook = categoryBooks.data.books;
 
     // 카테고리 타이틀
-    const categoryName = document.getElementById("categoryName")
-    categoryName.innerText = `${categoryBook.category}`
-
+    const categoryName = document.getElementById('categoryName');
+    categoryName.innerText = `${categoryBook.category}`;
 
     // 리스트에 뿌려지는 아이템 갯수
-    const totalText = document.getElementById("totalCount")
-    totalText.innerText = `${categoryBook.productCount} products`
+    const totalText = document.getElementById('totalCount');
+    totalText.innerText = `${categoryBook.productCount} products`;
 
     // 카테고리별 리스트
     const productListElement = document.getElementById('productList');
-    const books = categoryBook.books
+    const books = categoryBook.books;
 
-    books.slice(0, 16).forEach(book => {
-        // 상품 요소 생성
-        const productItem = document.createElement('li');
-        productItem.classList.add('product-item');
-        productItem.innerHTML = `
+    books.slice(0, 16).forEach((book) => {
+      // 상품 요소 생성
+      const productItem = document.createElement('li');
+      productItem.classList.add('product-item');
+      productItem.innerHTML = `
             <a href="/src/views/detail/detail.html?id=${book._id}">
                 <p class="img"><img src="${book.img_url}" alt="제품 이미지"></p>
                 <h2>${book.title}</h2>
@@ -47,53 +47,15 @@ async function getCategoryBooks() {
             </a>
         `;
 
-        // 상품 리스트에 상품 추가
-        productListElement.appendChild(productItem);
+      // 상품 리스트에 상품 추가
+      productListElement.appendChild(productItem);
     });
-
-
-
   } catch (error) {
     console.error('카테고리별 정보를 가져오는 중 오류가 발생했습니다:', error);
   }
 }
 
-/* All Books */
-async function getAllBooks() {
-  try {
-    // 서버에서 모든 책 목록을 가져오는 API 호출
-    const response = await fetch(`${API_HOST}/api/books`);
-    
-    // API 응답 데이터를 JSON 형태로 변환
-    const allBooks = await response.json();
-    console.log('모든 책 목록:', allBooks);
-
-    // 여기서 책 목록을 활용하여 원하는 작업을 수행할 수 있습니다.
-    const productListElement = document.getElementById('productList');
-
-    allBooks.slice(0, 16).forEach(book => {
-        // 상품 요소 생성
-        const productItem = document.createElement('li');
-        productItem.classList.add('product-item');
-        productItem.innerHTML = `
-            <a href="/src/views/detail/detail.html?id=${book._id}">
-                <p class="img"><img src="${book.img_url}" alt="제품 이미지"></p>
-                <h2>${book.title}</h2>
-                <span class="price">${book.price.toLocaleString()} 원</span>
-            </a>
-        `;
-
-        // 상품 리스트에 상품 추가
-        productListElement.appendChild(productItem);
-    });
-
-  } catch (error) {
-    console.error('모든 책 목록을 조회하는 중 오류가 발생했습니다:', error);
-  }
-}
-
 // 페이지가 로드되면 상품 리스트 렌더링
 window.addEventListener('load', () => {
-  getCategoryBooks()
-  getAllBooks()
-}); 
+  getCategoryBooks();
+});
