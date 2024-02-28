823ae7edc8a5505eac360d7c0037470b5feb48e2
alsgur39 <alsgur39@gmail.com>
2024-02-28 17:57:53 +0900

    Feat: 페이지네이션 구현완료

--- src/views/books/books.js
+++ src/views/books/books.js
@@ -1,18 +1,48 @@
 import { API_HOST } from '../common/api.js';
+let currentPage = 1;
+let totalPage = 1;
+
+
+// 페이지가 로드되면 상품 리스트 렌더링
+document.addEventListener('DOMContentLoaded', function () {
+  
+  // 페이지네이션 버튼 가져오기
+  const prevPageBtn = document.querySelector(".previous-page a");
+  const nextPageBtn = document.querySelector(".next-page a");
+
+  // 이전 페이지로 이동
+  prevPageBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+    if (currentPage > 1) {
+      currentPage--;
+      getAllBooks()
+    }
+  });
+
+  // 다음 페이지로 이동
+  nextPageBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+    if (currentPage < totalPage) {
+      currentPage++;
+      getAllBooks()
+    }
+  });
+});
+
 
 /* All Books */
-async function getAllBooks(allBooks) {
+async function getAllBooks() {
   try {
     // 서버에서 모든 책 목록을 가져오는 API 호출
-    const response = await fetch(`${API_HOST}/api/books`);
-
+    const response = await fetch(`${API_HOST}/api/books?page=${currentPage}`);
+    
+    
     // API 응답 데이터를 JSON 형태로 변환
     const allBooks = await response.json();
     console.log('모든 책 목록:', allBooks);
 
     const books = allBooks.data.books
-    console.log(books);
-
+    totalPage = allBooks.data.totalPage
     // 여기서 책 목록을 활용하여 원하는 작업을 수행할 수 있습니다.
     const productListElement = document.getElementById('productList');
 
@@ -20,6 +50,8 @@ async function getAllBooks(allBooks) {
     // 리스트에 뿌려지는 아이템 갯수
     const totalText = document.getElementById('totalCount');
     totalText.innerText = `${allBooks.data.productCount} products`;
+    
+    productListElement.innerHTML = '';
 
     books.slice(0, 24).forEach((book) => {
       // 상품 요소 생성
@@ -39,9 +71,15 @@ async function getAllBooks(allBooks) {
   } catch (error) {
     console.error('모든 책 목록을 조회하는 중 오류가 발생했습니다:', error);
   }
+  updatePagination();
 }
 
 // 페이지가 로드되면 상품 리스트 렌더링
 window.addEventListener('load', () => {
   getAllBooks();
 });
+
+function updatePagination() {
+  const currentPageInfo = document.getElementById("currentPageInfo");
+  currentPageInfo.textContent = `Page ${currentPage} of ${totalPage}`;
+}
