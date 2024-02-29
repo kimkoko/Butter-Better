b11ab64138791728bca56b39bf0633e1f54ecf90
Kang KyungRim <kkltr416@gmail.com>
2024-02-29 18:58:22 +0900

    Feat: 카테고리 관리 관리자 보기 / 추가 / 수정 기능 완료

--- src/views/admin/category/category.js
+++ src/views/admin/category/category.js
@@ -1,173 +1,245 @@
-
 import { API_HOST } from '../../common/api.js';
 
-document.addEventListener('DOMContentLoaded', function () {
-  // 모달 열기 버튼과 모달 가져오기
-  const addBtn = document.getElementById("add-btn");
-  const modal1 = document.getElementById("myModal1");
-  const modal2 = document.getElementById("myModal2");
 
-  addBtn.addEventListener("click", function ()  {
-    const pargraph = modal1.querySelector("p");
-    if (pargraph) {
-      pargraph.innerText = "카테고리 추가"
+// 카테고리 리스트
+// 카테고리 리스트를 불러오고 각 카테고리에 대한 요청을 보내는 함수
+async function getCategory() {
+    try {
+        const response = await fetch(`${API_HOST}/api/categories`);
+        const result = await response.json();
+        return result.data
+
+    } catch (error) {
+        console.error('카테고리 정보를 불러오는 중 오류 발생:', error);
     }
-    modal1.style.display = "flex";
-  });
-
-
-  // 모달 닫기 함수
-  modal1.addEventListener("click", function(e) {
-    if (e.target !== modal1) return;
-    modal1.style.display = "none";
-  })
-  modal2.addEventListener("click", function(e) {
-    if (e.target !== modal2) return;
-    modal2.style.display = "none";
-  })
-
-
-  // 저장 버튼에 이벤트 리스너 추가
-  const saveBtn1 = document.getElementById("save-Btn1");
-  saveBtn1.onclick = function () {
-    // 저장 버튼을 눌렀을 때 할 작업을 추가
-    modal1.style.display = "none";
-  };
-
-
-  // 삭제 버튼과 취소 버튼에 이벤트 리스너 추가
-  const deleteBtn = document.getElementById("delete-Btn");
-  const backBtn = document.getElementById("back-btn");
-
-  deleteBtn.onclick = function () {
-    // 실제로 삭제하는 작업 추가
-    alert("카테고리가 삭제되었습니다.");
-    modal2.style.display = "none";
-  };
-
-  backBtn.onclick = function () {
-    modal2.style.display = "none";
-  };
-});
-
-/////////////////////////////////////////////////////////////
-
-/* 수정 삭제 버튼 */
-function modalEditDelete () {
-  
-    const openModalBtn1 = document.querySelectorAll(".edit");
-    const openModalBtn2 = document.querySelectorAll(".delete");
-    const modal1 = document.getElementById("myModal1");
-    const modal2 = document.getElementById("myModal2");
-
-    openModalBtn1.forEach(openModalBtn1 => {
-      openModalBtn1.addEventListener("click", () => {
-        const pargraph = modal1.querySelector("p");
-        if (pargraph) {
-          pargraph.innerText = "카테고리 수정"
-        }
-        modal1.style.display = "flex";
-      })
-    })
-
-    openModalBtn2.forEach(openModalBtn2 => {
-      openModalBtn2.addEventListener("click", () => {
-        modal2.style.display = "flex";
-      });
-    })
 }
 
 
-/////////////////////////////////////////////////////////////
-
-
-/* 카테고리 데이터 불러오기 */
-async function fetchCategory() {
-  try {
-      // API에서 카테고리 데이터 가져오기
-      const response = await fetch(`${API_HOST}/api/categories`);
-      const res = await response.json();
-      return res.data
-
-  } catch (error) {
-      console.error('카테고리를  렌더링하는 중 오류가 발생했습니다:', error);
-  }
-}
-
-
-
-////////////////////////////////////////////////////////////
-/* 카테고리 데이터를 가져와서 메뉴를 렌더링하는 함수 */
+// 카테고리 리스트 뿌리기
 async function renderCategory() {
+    try{
+        const categories = await getCategory()
+        
+        console.log(categories)
+    
 
-  try {
-      const category = await fetchCategory()
-      const categoryListElement = document.getElementById('categoryList');
+        const categoryListElement = document.getElementById('categoryList');
 
-      
-      // 데이터를 기반으로 메뉴바 생성
-      category.forEach(category => {
-          // 메뉴 요소 생성
-          const categoryItem = document.createElement('tr');
-          categoryItem.classList.add('category');
-          categoryItem.innerHTML = `
+        // 카테고리 리스트를 받아온 후 각 카테고리에 대한 요청을 보냅니다.
+        categories.forEach(async (category) => {
+
+            
+            // 리스트 생성
+            const categoryItem = document.createElement('tr');
+            categoryItem.classList.add('category');
+            categoryItem.innerHTML = `
             <td>${category.name}</td>
             <td>${category.sort}</td>
             <td>
-                <button type="button" id="openModalbtn1" class="edit">수정</button>
-                <button type="button" id="openModalbtn2" class="delete">삭제</button>
+                <button type="button" id="openModalbtn1" class="edit" data-category-id="${category._id}">수정</button>
+                <button type="button" id="openModalbtn2" class="delete" data-category-id="${category._id}">삭제</button>
             </td>
-          `;
+            `;
 
-          // 상품 리스트에 상품 추가
-          categoryListElement.appendChild(categoryItem);
+            // 상품 리스트에 상품 추가
+            categoryListElement.appendChild(categoryItem);
 
-      });
-      
-      modalEditDelete()      
+            
 
-  } catch (error) {
-      console.error('카테고리를  렌더링하는 중 오류가 발생했습니다:', error);
-  }
+        });
+
+        
+
+        modalEditDelete()
+
+
+
+    } catch (error) {
+        console.error("카테고리를 렌더링하는 중 오류가 발생했습니다.", error)
+    }
+}
+
+
+//  카테고리 추가하기
+// 추가 버튼 이벤트
+function addButtonEvent () {
+
+    // 모달 열기 버튼과 모달 가져오기
+    const addBtn = document.getElementById("add-btn");
+    const modal1 = document.getElementById("myModal1");
+
+
+    // 카테고리 추가 모달 열기
+    addBtn.addEventListener("click", function ()  {
+        const pargraph = modal1.querySelector("p");
+        if (pargraph) {
+        pargraph.innerText = "카테고리 추가"
+        }
+        modal1.style.display = "flex";
+
+        // 초기화 함수
+        initializeForm()
+
+        // 저장 누르면 추가 함수 실행
+        const saveBtn1 = document.getElementById("save-Btn1");
+        saveBtn1.onclick = addCategory
+    });
+
+
+
+    // 모달 닫기 
+    modal1.addEventListener("click", function(e) {
+        if (e.target !== modal1) return;
+        modal1.style.display = "none";
+    })
+
+}
+
+
+// 추가 함수
+async function addCategory() {
+
+    const categoryName = document.getElementById('categoryInput')
+    const categorySort = document.getElementById('categorySort')
+    const modal1 = document.getElementById("myModal1");
+
+    try {
+        // 서버에 POST 요청을 보내어 카테고리 추가
+        const response = await fetch(`${API_HOST}/api/categories`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                name: categoryName.value,
+                sort: Number(categorySort.value)
+            })
+        });
+        
+        const result = await response.json();
+
+        if (response.ok) {
+            alert("새로운 카테고리가 추가되었습니다.");
+            modal1.style.display = "none";
+            window.location.reload()
+            renderCategory();
+        } else {
+            alert('상품 추가 실패');
+            console.log("상품 추가 실패", result)
+        }
+
+    } catch (error) {
+        console.error('카테고리 추가 중 오류가 발생했습니다:', error);
+    }
 }
 
 
 
-/////////////////////////////////////////////////////////////
-/* 카테고리 추가 */
-const categoryInput = document.getElementById('categoryInput');
-const addCategoryBtn = document.getElementById('save-Btn1');
-addCategoryBtn.addEventListener("click", async () => {
+// moal1 초기화
+function initializeForm() {
+    const categoryNameInput = document.getElementById("categoryInput");
+    const categorySortInput = document.querySelector("#myModal1 input[placeholder='순서, 0 부터 입력']");
+    
+    // 폼 필드 초기화
+    categoryNameInput.value = "";
+    categorySortInput.value = "";
+}
 
 
-  const categoryName = categoryInput.value
 
-  try {
-    const response = await fetchCategory(`${API_HOST}/api/category/admin`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        name: categoryName
-      })
-    })
+// 카테고리 수정하기
 
-    if (!response.ok) {
-      throw new Error("카테고리 추가에 실패했습니다.")
+// 카테고리 id로 해당 카테고리 찾기
+async function fetchCategoryById(categoryId) {
+    try {
+        const response = await fetch(`${API_HOST}/api/categories/${categoryId}`);
+        if (!response.ok) {
+            throw new Error('카테고리 정보를 가져오지 못했습니다.');
+        }
+        const category = await response.json();
+        console.log(category)
+
+        const categoryNameInput = document.getElementById("categoryInput");
+        const categorySortInput = document.querySelector("#myModal1 input[placeholder='순서, 0 부터 입력']");
+
+        // 폼에 카테고리 정보 채우기
+        categoryNameInput.value = category.data.name;
+        categorySortInput.value = category.data.sort;
+    } catch (error) {
+        console.error('카테고리 정보를 가져오는 중 오류 발생:', error);
+        throw error;
     }
-    alert("저장되었습니다.");
-  } catch (error) {
-    console.log("카테고리 추가 중 오류 발생", error)
-    alert("카테고리 추가 중 오류가 발생했습니다.")
-  }
-  
-})
+}
 
 
-// 페이지가 로드
+// 수정 삭제 클릭 이벤트
+function modalEditDelete() {
+    const editButtons = document.querySelectorAll('.edit');
+    const modal1 = document.getElementById("myModal1");
+
+    // 수정
+    editButtons.forEach(editButton => {
+        editButton.addEventListener('click', () => {
+            const paragraph = modal1.querySelector("p");
+            if (paragraph) {
+                paragraph.innerText = "카테고리 수정";
+            }
+            modal1.style.display = "flex";
+
+            const categoryId = editButton.dataset.categoryId; // 해당 id 불러오기
+            if (!categoryId) return;
+
+            const category = fetchCategoryById(categoryId)
+
+            // 저장 버튼에 이벤트 리스너 추가
+            const saveBtn1 = document.getElementById("save-Btn1");
+            saveBtn1.onclick = () => updateCategory(categoryId);
+
+            console.log(saveBtn1)
+        });
+    });
+}
+
+
+
+// 수정 폼에서 정보를 업데이트
+async function updateCategory(categoryId) {
+    const categoryNameInput = document.getElementById("categoryInput");
+    const categorySortInput = document.querySelector("#myModal1 input[placeholder='순서, 0 부터 입력']");
+    const modal1 = document.getElementById("myModal1");
+
+    const newName = categoryNameInput.value;
+    const newSort = categorySortInput.value;
+
+    try {
+        const response = await fetch(`${API_HOST}/api/categories/${categoryId}`, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            name: newName,
+            sort: newSort
+        })
+        });
+
+        if (!response.ok) {
+        throw new Error('카테고리 정보를 업데이트하지 못했습니다.');
+        }
+
+        // 카테고리 정보 업데이트 후 필요한 작업 수행
+        window.alert("수정을 완료했습니다.")
+        modal1.style.display = "none";
+        window.location.reload()
+        getCategory(); 
+    } catch (error) {
+        console.error('카테고리 정보를 업데이트하는 중 오류 발생:', error);
+    }
+    }
+
+// 페이지 로드
 window.addEventListener('load', () => {
-  fetchCategory()
-  renderCategory()
-
+    getCategory()
+    addButtonEvent()
+    renderCategory()
 }); 
