a07b44a0d0af7ef4f47d481b5746112029947a63
Saeyoung Choi <effysogood@gmail.com>
2024-03-01 04:07:37 +0900

    Style: Header 정렬

--- src/views/common/header/header.js
+++ src/views/common/header/header.js
@@ -3,107 +3,107 @@ import { waitFor } from '../../common/utils.js';
 
 // 카테고리 데이터 불러오기
 async function fetchCategory() {
-    try {
-        const response = await fetch(`${API_HOST}/api/categories`);
-        const res = await response.json();
-        return res.data;
-    } catch (error) {
-        console.error('카테고리를 렌더링하는 중 오류가 발생했습니다:', error);
-    }
+  try {
+    const response = await fetch(`${API_HOST}/api/categories`);
+    const res = await response.json();
+    return res.data;
+  } catch (error) {
+    console.error('카테고리를 렌더링하는 중 오류가 발생했습니다:', error);
+  }
 }
 
 // 유저 정보 불러오기
 async function fetchUser() {
-    const signOutBtn = document.querySelector('.button-signout');
-    const userBtn = document.querySelector('.button-user a');
-    const adminBtn = document.querySelector('.button-admin');
+  const signOutBtn = document.querySelector('.button-signout');
+  const userBtn = document.querySelector('.button-user a');
+  const adminBtn = document.querySelector('.button-admin');
 
-    try {
-        const response = await fetch(`${API_HOST}/api/users/mypage`);
-        const res = await response.json();
-        if (!response.ok) throw new Error();
+  try {
+    const response = await fetch(`${API_HOST}/api/users/mypage`);
+    const res = await response.json();
+    if (!response.ok) throw new Error();
 
-        signOutBtn.classList.remove('hidden');
-        userBtn.href = '/mypage';
+    signOutBtn.classList.remove('hidden');
+    userBtn.href = '/mypage';
 
-        res.user.is_admin
-            ? adminBtn.classList.remove('hidden')
-            : adminBtn.classList.add('hidden');
+    res.user.is_admin
+      ? adminBtn.classList.remove('hidden')
+      : adminBtn.classList.add('hidden');
 
-        return res;
-    } catch (error) {
-        signOutBtn.classList.add('hidden');
-        userBtn.href = '/login';
-        adminBtn.classList.add('hidden');
-    }
+    return res;
+  } catch (error) {
+    signOutBtn.classList.add('hidden');
+    userBtn.href = '/login';
+    adminBtn.classList.add('hidden');
+  }
 }
 
 // 카테고리 데이터를 가져와서 메뉴를 렌더링하는 함수
 async function renderCategory() {
-    try {
-        const category = await fetchCategory();
-        const categoryListElement = document.getElementById('categoryList');
+  try {
+    const category = await fetchCategory();
+    const categoryListElement = document.getElementById('categoryList');
 
-        category.forEach((category) => {
-            const categoryItem = document.createElement('li');
-            categoryItem.classList.add('menu');
-            categoryItem.innerHTML = `
+    category.forEach((category) => {
+      const categoryItem = document.createElement('li');
+      categoryItem.classList.add('menu');
+      categoryItem.innerHTML = `
             <a href="/category?category=${category._id}">${category.name}</a>
             `;
 
-            categoryListElement.appendChild(categoryItem);
-        });
-    } catch (error) {
-        console.error('카테고리를 렌더링하는 중 오류가 발생했습니다:', error);
-    }
+      categoryListElement.appendChild(categoryItem);
+    });
+  } catch (error) {
+    console.error('카테고리를 렌더링하는 중 오류가 발생했습니다:', error);
+  }
 }
 
 // 페이지가 로드되면 상품 리스트 렌더링
 document.addEventListener('DOMContentLoaded', async () => {
-    await waitFor(() => {
-        return !!document.getElementById('categoryList');
-    });
+  await waitFor(() => {
+    return !!document.getElementById('categoryList');
+  });
 
-    // 로그아웃 버튼 클릭 시
-    const signOutBtn = document.querySelector('.button-signout');
-    signOutBtn.addEventListener('click', async () => {
-        try {
-            const response = await fetch(`${API_HOST}/api/users/logout`);
-            const res = await response.json();
+  // 로그아웃 버튼 클릭 시
+  const signOutBtn = document.querySelector('.button-signout');
+  signOutBtn.addEventListener('click', async () => {
+    try {
+      const response = await fetch(`${API_HOST}/api/users/logout`);
+      const res = await response.json();
 
-            if (!response.ok) throw new Error(res.msg);
-            location.href = '/';
-        } catch (error) {
-            console.error('로그아웃 중 오류가 발생했습니다:', error);
-        }
-    });
-
-    // 유저 정보 불러오기
-    const user = await fetchUser();
-
-    // 로그인된 상태에서만 UI 업데이트
-    if (user && user.user) {
-        const userBtn = document.querySelector('.button-user a');
-        userBtn.href = '/mypage';
-
-        if (user.user.is_admin) {
-            const adminBtn = document.querySelector('.button-admin');
-            adminBtn.classList.remove('hidden');
-        }
+      if (!response.ok) throw new Error(res.msg);
+      location.href = '/';
+    } catch (error) {
+      console.error('로그아웃 중 오류가 발생했습니다:', error);
     }
+  });
 
-    // 장바구니 아이템 수 초기화
-    updateCartItemCount();
-    // 카테고리 렌더링
-    renderCategory();
+  // 유저 정보 불러오기
+  const user = await fetchUser();
+
+  // 로그인된 상태에서만 UI 업데이트
+  if (user && user.user) {
+    const userBtn = document.querySelector('.button-user a');
+    userBtn.href = '/mypage';
+
+    if (user.user.is_admin) {
+      const adminBtn = document.querySelector('.button-admin');
+      adminBtn.classList.remove('hidden');
+    }
+  }
+
+  // 장바구니 아이템 수 초기화
+  updateCartItemCount();
+  // 카테고리 렌더링
+  renderCategory();
 });
 
 // 장바구니 아이템 수 업데이트 함수
 function updateCartItemCount() {
-    const cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
+  const cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
 
-    const cartItemCountElement = document.querySelector('.num');
-    if (cartItemCountElement) {
-        cartItemCountElement.textContent = cartItems.length;
-    }
+  const cartItemCountElement = document.querySelector('.num');
+  if (cartItemCountElement) {
+    cartItemCountElement.textContent = cartItems.length;
+  }
 }
